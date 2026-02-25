05f614c257cc438f65752be4c0b5d0a6891f484a
WoIvie <calzan3@gmail.com>
2026-02-26 00:22:37 +0800

    Update image proxy to use weserv.nl as default

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,8 @@
-function processImageUrl(url) {
-    const encodedUrl = encodeURIComponent(url);
-    return `https://images.weserv.nl/?url=${encodedUrl}`;
-}
+// utils.ts
+
+const DEFAULT_IMAGE_PROXY = 'https://images.weserv.nl';
+
+export const getImageProxy = (imageUrl) => {
+    const imageProxy = process.env.IMAGE_PROXY || DEFAULT_IMAGE_PROXY;
+    return `${imageProxy}?url=${encodeURIComponent(imageUrl)}`;
+};
